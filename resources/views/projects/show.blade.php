17ead7e59c22b470cc33f6ff6e17ea0489bff9e5
Josue Cardona <ingjosue.cardona@gmail.com>
2026-05-19 12:00:17 -0600

    add img rev

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -12,7 +12,7 @@
         document.documentElement.dataset.editorialTheme = theme;
       })();
     </script>
-    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-2">
+    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-3">
   </head>
   <body>
     <canvas id="ember-canvas" class="motion-lines-canvas" aria-hidden="true"></canvas>
