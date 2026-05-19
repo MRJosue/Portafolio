327452ee47f36472eefd0c4fb38b356e77476d7b
Josue Cardona <ingjosue.cardona@gmail.com>
2026-05-19 12:24:07 -0600

    test

--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -12,7 +12,7 @@
         document.documentElement.dataset.editorialTheme = theme;
       })();
     </script>
-    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-7">
+    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-9">
   </head>
   <body class="journal-page">
     <canvas id="ember-canvas" class="motion-lines-canvas" aria-hidden="true"></canvas>
--- resources/views/themes/editorial-black.blade.php
+++ resources/views/themes/editorial-black.blade.php
@@ -13,7 +13,7 @@
         document.documentElement.dataset.editorialTheme = theme;
       })();
     </script>
-    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-7">
+    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-9">
   </head>
   <body>
     <canvas id="ember-canvas" class="motion-lines-canvas" aria-hidden="true"></canvas>
