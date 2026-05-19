f9192aa33038a1be4e3edfe95f97ab0cc4aef91c
Josue Cardona <ingjosue.cardona@gmail.com>
2026-05-19 12:04:31 -0600

    test

--- resources/views/themes/editorial-black.blade.php
+++ resources/views/themes/editorial-black.blade.php
@@ -13,7 +13,7 @@
         document.documentElement.dataset.editorialTheme = theme;
       })();
     </script>
-    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-3">
+    <link rel="stylesheet" href="{{ asset('css/editorial-black.css') }}?v=project-images-4">
   </head>
   <body>
     <canvas id="ember-canvas" class="motion-lines-canvas" aria-hidden="true"></canvas>
